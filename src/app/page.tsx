698172eab9ebb13d9adcf994743748dec588558e
Divya Shah <ds3636005@gmail.com>
2024-06-23 21:29:59 +0530

    update: update homepage and contact page path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Homepage from "@/components/Homepage";
+import Homepage from "@/components/main/Homepage";
 
 export default function Home() {
   return (
     <main>
       <div id="about" className="text-center h-screen bg-white bg-opacity-30">
-        <Homepage/>
+        {/* <Homepage/> */}
       </div>
       <div id="projects" className="text-center bg-black h-screen">
         Projects
