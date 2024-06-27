77659ab58e04f7b0d6315f2c1c7287f11fefe0ef
Divya Shah <ds3636005@gmail.com>
2024-06-27 10:00:23 +0530

    update: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(glb|gltf|png|jpg|jpeg)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/images/",
+          outputPath: "static/images/",
+          name: "[name].[hash].[ext]",
+          esModule: false,
+        },
+      },
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
